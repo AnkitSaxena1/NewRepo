0388b1e6c1a632bdd560f68afc0e334fad92d24d
Ankit Saxena <ankit.saxena@vivo.ooo>
2021-01-21 12:38:27 +0530

    New Func

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             
-            // changes from github 1.
+            // Use class user here
         }
     }
 }
